9c77d9f6ca4fd4deca361ad88720eb5a8ca7fcc2
devplus-quypham <quypham.devplus@gmail.com>
2022-03-15 11:29:17 +0700

    feature delete

--- backend/routes/route.js
+++ backend/routes/route.js
@@ -58,3 +58,17 @@ router
       }
     );
   });
+// Delete Student
+router.delete("/delete-student/:id", (req, res, next) => {
+  studentSchema.findByIdAndRemove(req.params.id, (error, data) => {
+    if (error) {
+      return next(error);
+    } else {
+      res.status(200).json({
+        msg: data,
+      });
+    }
+  });
+});
+
+module.exports = router;
